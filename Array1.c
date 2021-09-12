2e7f174b10fca1128f1a1fa5ecf84ea3ec398c6a
Sonu Paswan <sonupas2806@gmail.com>
2021-09-12 18:07:15 +0530

    initial commit

---
+++ Array1.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+int length;
+void sort(int A[], int len)
+{
+    for (int i = 1; i < len; i++)
+    {
+        int key = A[i];
+        int j = i - 1;
+        while (j >= 0 && key < A[j])
+        {
+            A[j + 1] = A[j];
+            j--;
+        }
+        A[j + 1] = key;
+    }
+}
+void PrintArray(int A[], int len)
+{
+    printf("printing original elements of array\n");
+    for (int i = 0; i < len; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+void PrintInReverse(int A[], int len)
+{
+    printf("printing elements in reverse order\n");
+    for (int i = len - 1; i >= 0; i--)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+void PrintAscending(int A[], int size)
+{
+    printf("printing elements in ascending order\n");
+    int array[size];
+    for (int i = 0; i < size; i++)
+        array[i] = A[i];
+    sort(array, size);
+    for (int i = 0; i < size; i++)
+        printf("%d ", array[i]);
+    printf("\n");
+}
+void Delete(int A[], int len, int pos)
+{
+    if (pos > len - 1)
+        printf("array out of bound\n");
+    else
+    {
+        printf("deleted element is %d\n", A[pos]);
+        for (int i = pos; i < len - 1; i++)
+        {
+            A[i] = A[i + 1];
+        }
+        length--;
+    }
+}
+
+int main()
+{
+    int arr[] = {2, 3, 5, 8, 45, 23, 12, 90, 36, 83, 10};
+    length = sizeof(arr) / sizeof(arr[0]);
+    int position = 5;
+    PrintArray(arr, length);
+    PrintAscending(arr, length);
+    PrintInReverse(arr, length);
+    Delete(arr, length, position);
+    PrintArray(arr, length);
+    return 0;
+}
---
+++ Array2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int size;
+int Sum(int A[], int len)
+{
+    printf("sum of all elemets of arrays\n");
+    int sum = 0;
+    for (int i = 0; i < len; i++)
+        sum += A[i];
+    return sum;
+}
+float Avergae(int A[], int len)
+{
+    printf("average of all elements of arrays\n");
+    float add = 0;
+    for (int i = 0; i < len; i++)
+        add += A[i];
+    return (float)add / len;
+}
+void Duplicate(int A[], int len)
+{
+    printf("duplicate numbers in array\n");
+    int a = 0;
+    int flag = 0;
+    for (int i = 0; i < len; i++)
+    {
+        a = A[i];
+        flag = 0;
+        for (int j = i + 1; j < len; j++)
+        {
+            if (a == A[j])
+                flag = 1;
+        }
+        if (flag == 1)
+            printf("%d ", a);
+    }
+}
+int main()
+{
+    int arr[] = {3, 2, 1, 7, 6, 6, 5, 4, 8, 9, 9};
+    size = sizeof(arr) / sizeof(arr[0]);
+    printf("%d\n", Sum(arr, size));
+    printf("%f\n", Avergae(arr, size));
+    Duplicate(arr, size);
+    return 0;
+}
