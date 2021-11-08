fa70cb7c869fe87a1a3fc7c48e737a9881e092b3
Sonu Paswan <88018101+sonustein@users.noreply.github.com>
2021-11-08 17:21:08 +0530

    Create program1.c

---
+++ DSA Assignment 5/program1.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+    int len;
+    printf("how many numbers ? ");
+    scanf("%d", &len);
+    int *arr = (int *)malloc(sizeof(int) * len);
+    printf("enter numbers\n");
+    for (int i = 0; i < len; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    /*Bubble sort algorithm*/
+    int indicator;
+    for (int i = 0; i < len - 1; i++)
+    {
+        indicator = 0;
+        for (int j = 0; j < len - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr + j, arr + j + 1);
+                indicator = 1;
+            }
+        }
+        if (indicator == 0)
+        {
+            // printf("already sorted !");
+            break;
+        }
+    }
+
+    printf("\nafter sorting\n");
+    for (int j = 0; j < len; j++)
+    {
+        printf("%d ", arr[j]);
+    }
+    return 0;
+}
